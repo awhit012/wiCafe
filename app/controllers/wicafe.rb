3a8efa8c371f54e7417e835c858fd29b6176c62e
Alex White <awhit012@gmail.com>
2014-07-27 15:28:31 -0700

    fixes signup form to save to db

--- app/controllers/wicafe.rb
+++ app/controllers/wicafe.rb
@@ -3,21 +3,30 @@ enable :sessions
 get '/' do
   # p "GET: / - Root Homepage"
   if logged_in?
-  	redirect( "/#{current_user.username}/notes" )
-  else 
+  	redirect( "/#{current_user.username}/profile" )
+  else
  		erb :index
   end
 end
 
 
-get '/cafes' do 
+get '/cafes' do
 	erb :cafes
 end
 
 post '/users/create' do
-  "Hello World"
+  @user = User.create(name: params[:name], email: params[:email])
+  @user.password_hash = params[:password_hash]
+  @user.save!
+
+  redirect '/'
 end
 
 post '/users/login' do
-  "Hello World"
+  #sign-in, create session
+  @user = User.where(email: params[:email]).first
+  if @user.password == params[:password_hash]
+    session[:user_id] = @user.user_id
+  end
+  redirect( "/#{current_user.username}/profile" )
 end
